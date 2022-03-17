ad5fbf51435ef82a01f2a55ce549b7575e90db27
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-03-18 00:41:22 +0300

    solved

--- songr/src/test/java/com/example/songr/SongrApplicationTests.java
+++ songr/src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.songr;
 
+import com.example.songr.domain.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
 class SongrApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void AlbumTest()
+	{
+		List<Album> allAlbums = new ArrayList<>();
+		Album myAlbum=  new Album("Al-Mu'allim","Sami Yusuf",8,5,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZvHHro_2CB3jf1Irc4wUoyPqjjTkWAUfICQ&usqp=CAU");
+		allAlbums.add(myAlbum);
+		assertTrue(myAlbum.getArtist()== "Sami Yusuf");
+	}
+	@Test
+	void AlbumTest2()
+	{
+		List<Album> allAlbums = new ArrayList<>();
+		Album myAlbum=  new Album("Al-Mu'allim","Sami Yusuf",8,5,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZvHHro_2CB3jf1Irc4wUoyPqjjTkWAUfICQ&usqp=CAU");
+		allAlbums.add(myAlbum);
+		assertTrue(myAlbum.getTitle() =="Al-Mu'allim");
+	}
+	@Test
+	void AlbumTest3()
+	{
+		List<Album> allAlbums = new ArrayList<>();
+		Album myAlbum=  new Album("Al-Mu'allim","Sami Yusuf",8,5,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZvHHro_2CB3jf1Irc4wUoyPqjjTkWAUfICQ&usqp=CAU");
+		allAlbums.add(myAlbum);
+		assertTrue(myAlbum.getLength() ==5);
+	}
+	@Test
+	void AlbumTest4()
+	{
+		List<Album> allAlbums = new ArrayList<>();
+		Album myAlbum=  new Album("Al-Mu'allim","Sami Yusuf",8,5,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZvHHro_2CB3jf1Irc4wUoyPqjjTkWAUfICQ&usqp=CAU");
+		allAlbums.add(myAlbum);
+		assertTrue(myAlbum.getSongCount() ==8
+		);
 	}
 
 }
