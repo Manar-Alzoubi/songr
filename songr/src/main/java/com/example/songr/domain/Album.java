91a380b9b2df97a0f1bf5f005b55345bde8ef52b
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-03-23 14:45:15 +0300

    lab12 updated setters added

--- songr/src/main/java/com/example/songr/domain/Album.java
+++ songr/src/main/java/com/example/songr/domain/Album.java
@@ -50,6 +50,30 @@ public class Album {
         return imageUrl;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     @Override
     public String toString() {
         return "Album{" +
